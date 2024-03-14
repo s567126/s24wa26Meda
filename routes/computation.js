0a198a2bc33c31d3038f6e428964309f0dc5a021
s567126 <156241739+s567126@users.noreply.github.com>
2024-03-14 16:45:11 -0500

    done

---
+++ routes/computation.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    var x = Math.floor(Math.random());
+    var y = Math.floor(Math.random());
+
+    if (req.query.x) {
+        x = (req.query.x);
+    }
+
+    function calculatingCeil(x, y) {
+        return (Math.ceil(x, y));
+    }
+
+
+    console.log(calculatingCeil(x,y));
+
+    res.send(`Math.ceil applied on ${x} and ${y} is ${calculatingCeil(x, y)}`)
+});
+
+module.exports = router;
