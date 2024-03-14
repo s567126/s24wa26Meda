6ed304254afd551e943d55e826ec1f1adc7ced6b
s567126 <156241739+s567126@users.noreply.github.com>
2024-03-14 15:59:22 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var product = 1;
+var factor = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  factor++;
+  product = product * factor;
+  res.send('Product is:' + factor);
   res.send('respond with a resource');
 });
 
